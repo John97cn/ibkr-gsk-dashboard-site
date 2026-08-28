2d5de1a024c3a2d4681cbc344ce215e543379233
John97cn <John97cn@users.noreply.github.com>
2026-08-28 15:16:15 +0800

    Deploy period return summary fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "ibkr-gsk-dashboard-v9";
+const CACHE = "ibkr-gsk-dashboard-v10";
 const CORE = ["./", "./index.html", "./manifest.webmanifest", "./icons/icon-192.png"];
 
 self.addEventListener("install", (event) => {
