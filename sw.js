cefef1b3c68ea9b17349f779a500e3dda13e017e
John97cn <John97cn@users.noreply.github.com>
2026-08-28 10:18:48 +0800

    Deploy holding metrics redesign

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "ibkr-gsk-dashboard-v7";
+const CACHE = "ibkr-gsk-dashboard-v8";
 const CORE = ["./", "./index.html", "./manifest.webmanifest", "./icons/icon-192.png"];
 
 self.addEventListener("install", (event) => {
